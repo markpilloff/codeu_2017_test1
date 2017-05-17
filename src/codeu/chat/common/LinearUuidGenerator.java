a2268e63f82b4d738ae65e91d2d94d928181e0d9
Mark <Mark@Mark-PC>
2017-05-16 20:41:22 -0700

    simple comment

--- src/codeu/chat/common/LinearUuidGenerator.java
+++ src/codeu/chat/common/LinearUuidGenerator.java
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Just a comment added by Mark...
 package codeu.chat.common;
 
 import codeu.chat.util.Uuid;
